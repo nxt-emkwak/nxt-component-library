6152c03d25426bc2087a50449ee3654559baea4b
Emily <emilyk@nutranext.net>
2020-07-21 16:54:04 -0400

    update styling

--- packages/Button/src/Button.styles.js
+++ packages/Button/src/Button.styles.js
@@ -2,17 +2,18 @@ import styled from "styled-components";
 
 const StyledButton = styled.button`
   min-width: 64px;
-  color: '#e0e0e0;
+  color: #fff;
+  background-color: #eb34a1;
   font-size: 0.875rem;
-  font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+  font-family: "Roboto", "Helvetica", "Arial", sans-serif;
   font-weight: 500;
   line-height: 1.75;
   letter-spacing: 0.02857em;
   margin: 1em;
   padding: 6px 16px;
   box-sizing: border-box;
+  border: none;
   border-radius: 4px;
   text-transform: uppercase;
-  background-color: '#e0e0e0';
 `;
 export default StyledButton;
