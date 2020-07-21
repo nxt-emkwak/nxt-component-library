c3a9dba66f625a3f460a2cca02e2a5daadb657b5
Emily <emilyk@nutranext.net>
2020-07-21 15:58:21 -0400

    config button package

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,4 @@
-const custom = require("../webpack.config.js");
-
 module.exports = {
-  stories: ["../src/**/*.stories.js"],
-  addons: ["@storybook/addon-actions", "@storybook/addon-links"],
-  webpackFinal: async (config) => {
-    return {
-      ...config,
-      module: { ...config.module, rules: custom.module.rules },
-    };
-  },
+  stories: ["../packages/**/*.stories.js"],
+  addons: ["@storybook/addon-docs"],
 };
---
+++ packages/Button/src/Button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "./index.js";
+
+export default {
+  title: "Button",
+  component: Button,
+};
+
+export const Button1 = () => <Button>Default</Button>;
