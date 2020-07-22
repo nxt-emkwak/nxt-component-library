e3588e5506b0bd4844c1b5e7f1e367f8bee8156f
Emily <emilyk@nutranext.net>
2020-07-22 12:27:55 -0400

    add theme

--- packages/nxt-comp/src/Input/Input.js
+++ packages/nxt-comp/src/Input/Input.js
@@ -1,14 +1,20 @@
 import React from "react";
 
+import { ThemeProvider } from "styled-components";
+import theme from "../Theme/Theme";
 import StyledInput from "./Input.styles.js";
 
-const Input = ({ disabled, id, label, placeholder }) => (
-  <StyledInput
-    disabled={disabled}
-    id={id}
-    placeholder={placeholder}
-    type="text"
-  />
+const Input = ({ primary, secondary, disabled, id, label, placeholder }) => (
+  <ThemeProvider theme={theme}>
+    <StyledInput
+      disabled={disabled}
+      id={id}
+      placeholder={placeholder}
+      type="text"
+      primary={primary}
+      secondary={secondary}
+    ></StyledInput>
+  </ThemeProvider>
 );
 
 export default Input;
--- packages/nxt-comp/src/Input/Input.styles.js
+++ packages/nxt-comp/src/Input/Input.styles.js
@@ -1,7 +1,13 @@
 import styled from "styled-components";
 
 const StyledInput = styled.input`
-  border: 1px solid #ccc;
+  border: 1px solid
+    ${(props) =>
+      props.primary
+        ? props.theme.palette.primary.main
+        : props.secondary
+        ? props.theme.palette.secondary.main
+        : "#bdbdbd"};
   border-radius: 4px;
   box-sizing: border-box;
   font-size: 16px;
