16ac83ecc3c34d1c67fda129d3087fb0bf79d5b0
Emily <emilyk@nutranext.net>
2020-07-22 19:58:01 -0400

    add className

--- packages/nxt-comp/src/Input/Input.js
+++ packages/nxt-comp/src/Input/Input.js
@@ -4,9 +4,18 @@ import { ThemeProvider } from "styled-components";
 import theme from "../Theme/Theme";
 import StyledInput from "./Input.styles.js";
 
-const Input = ({ primary, secondary, disabled, id, label, placeholder }) => (
+const Input = ({
+  className,
+  primary,
+  secondary,
+  disabled,
+  id,
+  label,
+  placeholder,
+}) => (
   <ThemeProvider theme={theme}>
     <StyledInput
+      className={className}
       disabled={disabled}
       id={id}
       placeholder={placeholder}
